ec7685b8d5abe30b7cd65cf097bcd00b3e1acc0a
cucudas01 <4996yys@naver.com>
2026-02-10 03:02:30 +0900

    AndroidManifest.xml에서 사용한 ${mapsApiKey} 변수를 안드로이드 빌드 시스템에 연결해주는 코드 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        manifestPlaceholders["mapsApiKey"] = localProperties.getProperty("mapsApiKey") ?: ""
     }
 
     buildTypes {
